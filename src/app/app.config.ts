4c0269254accfbcf7361aa88acbd5c3359ae6b20
Platon3003 <plogvinenko05@gmail.com>
2024-04-17 08:11:03 +0200

    configs and auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
+// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; // Import HttpClientModule
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), HttpClient] // Provide HttpClientModule
 };
