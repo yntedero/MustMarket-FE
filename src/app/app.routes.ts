41524354bb28e03a72a2f7a0f815fb94e62b6726
yntedero <yntedero@gmail.com>
2024-04-21 10:54:09 +0200

    Deleting all project

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { OffersComponent } from './offers/offers.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'offers', component: OffersComponent },
-];
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { RootModule } from './app/root.module';
-
-platformBrowserDynamic().bootstrapModule(RootModule)
-  .catch(err => console.error(err));
