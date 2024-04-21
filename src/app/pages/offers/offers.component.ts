3ee2ff36c177cc3a71aec5bf1ff778e8c4ab9b35
Platon3003 <plogvinenko05@gmail.com>
2024-04-21 17:16:54 +0200

    added offers

---
+++ src/app/pages/offers/offers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers',
+  standalone: true,
+  imports: [],
+  templateUrl: './offers.component.html',
+  styleUrl: './offers.component.scss'
+})
+export class OffersComponent {
+
+}
